043b60705327cfdf62e79eb8d2b933d709b6859a
Цепелёв <kirzep@gmail.com>
2024-02-12 05:21:11 +0300

    Change greeting function name

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 import brainEvenGame from '../games/game-brain-even.js';
-import Greetings from '../src/cli.js';
-import { GameResult, getGameRules } from '../src/index.js';
+import greetings from '../src/cli.js';
+import { getGameResult, getGameRules } from '../src/index.js';
 
-const name = Greetings();
+const name = greetings();
 getGameRules('brain-even');
-GameResult(brainEvenGame(), name);
+getGameResult(brainEvenGame(), name);
